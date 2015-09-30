23fe5f4483400a8f8949642de8ce1260c1eb2230
Adam Rogers <arogers0311@gmail.com>
2015-09-30 16:07:33 +1000

    commit code

---
+++ src/dnx-linux-execute/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace dnx_linux_execute
+{
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+            var startInfo = new ProcessStartInfo();
+            startInfo.FileName = "uname";
+            startInfo.Arguments = "-a";
+            startInfo.UseShellExecute = true;
+
+            try
+            {
+                // Start the process with the info we specified.
+                // Call WaitForExit and then the using-statement will close.
+                using (var process = Process.Start(startInfo))
+                {
+                    process.WaitForExit();
+                }
+            }
+            catch
+            {
+                throw new NotSupportedException();
+            }
+        }
+    }
+}
